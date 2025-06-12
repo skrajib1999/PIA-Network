a1322cdd02c121e7300e3f52156b6711788d4747
skrajib1999 <117132221+skrajib1999@users.noreply.github.com>
2025-06-12 17:31:22 +0600

    Delete PIA Network directory

--- PIA Network/backend/app/schemas/__init__.py
+++
@@ -1,8 +0,0 @@
-from .user import *
-from .auth import *
-from .mining import *
-from .task import *
-from .referral import *
-from .wallet import *
-
-from app.schemas import UserCreate, Token, MiningStartRequest
